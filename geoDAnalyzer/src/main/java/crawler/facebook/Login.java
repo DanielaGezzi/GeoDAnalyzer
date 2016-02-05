36959bd68ff16ab581803128ef4410a932687f74
Daniela <Daniela@DESKTOP-2FBT2UR>
2016-02-05 12:28:18 +0100

    delete pw

--- geoDAnalyzer/src/main/java/crawler/facebook/Login.java
+++ geoDAnalyzer/src/main/java/crawler/facebook/Login.java
@@ -10,9 +10,9 @@ public class Login {
 		cb.setDebugEnabled(true);
 		cb.setJSONStoreEnabled(true);
 		
-		cb.setOAuthAppId("634907736647778");
-		cb.setOAuthAppSecret("b83f3acfe102420946fc0517a7dd14ef");
-		cb.setOAuthAccessToken("CAACEdEose0cBABHWyvZAqAdurYNj0kkdZBzHu68uYiM3HWuHvSxVtZC8YDBisg3f31DgSAxknm2zup7NFcZBqCiZAWaEu6JM403GqSJ0DiuH6LZARcuH79fZAJXjKjr1s269e6vqpNGEFScqgANY4HWR3mZCTbZCQV96gZBKmh49EmLk1MiWaSkAPJ8Nyg4ZAsPz54e0W5ZB3K88JwZDZD");
+		cb.setOAuthAppId("*");
+		cb.setOAuthAppSecret("*");
+		cb.setOAuthAccessToken("*");
 		cb.setOAuthPermissions("events");
 
 		return cb;
